0fc65de0b75ff0dbe4dd725b8f7144245ff466c6
Martin Joiner <martin@martinjoiner.co.uk>
2013-02-27 21:29:10 +0000

    Javascript for What Did You Kill page

---
+++ js/whatdidyoukill.js
@@ -0,0 +1,11 @@
+
+// Load JSON
+for(animal in animals){
+
+	console.log(animals[animal].score);
+}
+
+// Print row in form for each animal
+
+// Attach event for onclick of animat { Play noise, increase total }
+
