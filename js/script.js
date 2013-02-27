9c32119d5325a85dbe18e777904c53c7bff542d1
darren <darren@darren-netbook.isofin.co.uk>
2013-02-27 21:05:07 +0000

    renamed animals added animation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(function(){    
-      animateDiv();
+     animateDiv();
+    var animals = ['cow','sheep','swan','dolphin','horse'];
     var height = this.height;
     var width = this.width;
     var xPos;
@@ -27,19 +28,29 @@ function makeNewPosition(){
     
     // Get viewport dimensions (remove the dimension of the div)
     var h = $(window).height() - 50;
-    var w = $(window).width() - 50;
-    
+    var w = $(window).width() - 50;    
     var nh = Math.floor(Math.random() * h);
-    var nw = Math.floor(Math.random() * w);
-    
+    var nw = Math.floor(Math.random() * w);    
     return [nh,nw];    
     
 }
 
-function animateDiv(animals){
+function animateDiv(){
     var newq = makeNewPosition();
-    $('.a').animate({ top: newq[0], left: newq[1] }, function(){
+    var oldq = $('.cow').offset();
+    var speed = calcSpeed([oldq.top, oldq.left], newq);
+    
+    $('.cow').animate({ top: newq[0], left: newq[1] }, speed, function(){
       animateDiv();        
     });
     
-};
+}
+
+function calcSpeed(prev, next) {    
+    var x = Math.abs(prev[1] - next[1]);
+    var y = Math.abs(prev[0] - next[0]);    
+    var greatest = x > y ? x : y;    
+    var speedModifier = 0.1;
+    var speed = Math.ceil(greatest/speedModifier);
+    return speed;
+}
